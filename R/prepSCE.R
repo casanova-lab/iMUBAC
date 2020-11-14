26f341256224a0fc6bdf7294ab40bf6dfc84f888
Masato Ogishi <22980126+masato-ogishi@users.noreply.github.com>
2020-11-14 15:12:53 -0500

    minor update

--- R/prepSCE.R
+++ R/prepSCE.R
@@ -128,10 +128,11 @@ prepSCE <- function(
       })
     }else if(identical(type, "Flow")){
       cat("-Logicle transformation...\n", sep="")
+      tmax <- flowCore::fsApply(fs, function(ff){max(ff@exprs)})
       fs <- flowCore::fsApply(fs, function(ff){
         trans <- flowCore::transformList(
           from=setdiff(chs, chs_ex),
-          tfun=flowCore::logicleTransform(w=0.5, t=max(ff@exprs), m=4.5, a=0)
+          tfun=flowCore::logicleTransform(w=0.5, t=tmax, m=4.5, a=0)
         )
         #trans <- flowCore::estimateLogicle(ff, channels=setdiff(chs, chs_ex), t=max(ff@exprs))
         ff <- flowCore::transform(ff, trans)
